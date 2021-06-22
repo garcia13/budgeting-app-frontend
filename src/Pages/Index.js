eb56abcca5f5564482ec2f6a6b649453b44864be
Miguel Garcia <miguelgarcia@pursuit.org>
2021-06-21 21:50:49 -0400

    numbers are not strings

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -7,7 +7,7 @@ export default function Index({ transactionsArr }) {
     
     let total = 0
     for (let i = 0; i < transactionsArr.length; i++) {
-     total += transactionsArr[i].amount
+     total += Number(transactionsArr[i].amount)
     }
     return total
   };
